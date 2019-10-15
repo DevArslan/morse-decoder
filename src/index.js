174406fd407db45527638d16aab44ff19de2cea3
DevArslan <arslan.gareev.2017@gmail.com>
2019-10-15 22:21:36 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,88 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+    morseArray = []
+    complete_array = []
+    sliceBinaryArray = slice_array(expr)
+    morseArray = binary_to_morse(sliceBinaryArray)
+    
+
+    for (var key in morseArray){
+        if (morseArray[key] in MORSE_TABLE){
+            complete_array.push(MORSE_TABLE[morseArray[key]])
+        }
+        if (morseArray[key] == ' '){
+            complete_array.push(' ')
+        }
+
+        
+    }
+    
+    return complete_array.join('')
+    
+   
+}
+
+function slice_array(binaryArray) {
+    sliceBinaryArray = []
+    for (let i = 0; i < binaryArray.length;){
+        sliceBinaryArray.push(binaryArray.slice(i, i+10));
+        i = i+10;
+    }
+    
+    let complete_array = sliceBinaryArray
+    return complete_array
+}
+
+
+function binary_to_morse(sliceBinaryArray) {
+
+    let morseArray = []
+
+    for ( let i = 0; i < sliceBinaryArray.length; i++){
+
+        sliceBinaryArrayItem = sliceBinaryArray[i];
+        
+        for ( let k = 0; k < 10; k+=2){
+            
+
+            
+            
+            if (sliceBinaryArrayItem[k] == '1' && sliceBinaryArrayItem[k+1] == '0'){
+
+                morseArray[i] += '.'
+    
+            }
+            if (sliceBinaryArrayItem[k] == '1' && sliceBinaryArrayItem[k+1] == '1'){
+    
+                morseArray[i] += '-'
+    
+            }
+            if (sliceBinaryArrayItem[k] == '0' && sliceBinaryArrayItem[k+1] == '0'){
+    
+                morseArray[i] += ''
+    
+            }
+            if (sliceBinaryArrayItem[k] == '*' && sliceBinaryArrayItem[k+1] == '*'){
+    
+                morseArray[i] += ' ';
+                k = k + 9;
+    
+            }
+            
+
+        }
+
+        
+        morseArray[i] = morseArray[i].replace('undefined','')
+        
+    }
+    
+    return morseArray;
+        
 }
 
 module.exports = {
